e322942f20d2017781d26a59d77679d0713cfbbe
florencia <florenciazuazaga47@gmail.com>
2023-10-31 10:52:54 -0300

    node_modules

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const exphbs = require('express-handlebars');
+const handlebars = require('express-handlebars');
 const path = require('path');
 const fs = require('fs');
 
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 8080;
 
 // Configuración de Handlebars
 app.engine('handlebars', handlebars.engine());
+
+
+
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
