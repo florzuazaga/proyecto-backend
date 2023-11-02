85e6b60bece8576ea0ca2cb7b6e08ac6307c4b5c
florencia <florenciazuazaga47@gmail.com>
2023-11-02 09:42:45 -0300

    codeunico

--- routes.js
+++ routes.js
@@ -7,6 +7,29 @@ const ProductManager = require('./ProductManager'); // Asegúrate de que la impo
 // Crear una instancia de ProductManager
 const productManager = new ProductManager(/* ruta al archivo de productos */);
 
+// Ruta para crear un nuevo producto
+router.post('/', (req, res) => {
+  try {
+    const { name, price, code } = req.body;
+
+    // Validación de campos obligatorios
+    if (!name || !price || !code) {
+      return res.status(400).json({ error: 'Nombre, precio y código son campos obligatorios.' });
+    }
+
+    // Validación de campo code único
+    const existingProduct = productManager.getProducts().find((product) => product.code === code);
+    if (existingProduct) {
+      return res.status(400).json({ error: 'El código del producto ya existe.' });
+    }
+
+    const newProduct = productManager.addProduct(req.body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 // Definir rutas
 router.get('/', (req, res) => {
   // Ruta para mostrar todos los productos
