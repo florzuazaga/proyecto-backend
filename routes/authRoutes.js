3bc02672eba325291266c57d086e9cc6bedde802
florencia <florenciazuazaga47@gmail.com>
2023-12-11 10:12:50 -0300

    instalaciones

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { authenticateUser } = require('../controllers/authController');
 const router = express.Router();
 
+
 router.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
