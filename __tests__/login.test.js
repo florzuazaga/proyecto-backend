001453e6758cf03d6e99ebf904fa8e7c4bd1b7b1
florencia <florenciazuazaga47@gmail.com>
2024-02-14 17:38:31 -0300

    cambios en winston

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,53 +1,51 @@
 // login.test.js
+// login.test.js
 const request = require('supertest');
 const express = require('express');
 const mongoose = require('mongoose'); 
+const winston = require('winston');
 const authRoutes = require('../routes/authRoutes');
-const { User } = require('../dao/models/userSchema');
 
 const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
 
+const logger = winston.createLogger({
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console({ level: 'info' }),  // Nivel de prioridad para consola
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),  // Archivo para errores
+    new winston.transports.File({ filename: 'combined.log' })  // Archivo para mensajes informativos
+  ],
+});
+
+const registerUser = async () => {
+  return await request(app)
+    .post('/auth/register')
+    .send({
+      nombre: 'John',
+      apellido: 'Doe',
+      email: 'john.doe@example.com',
+      contraseña: 'password123',
+      username: 'john_doe',
+    });
+};
+
 describe('Authentication Endpoints', () => {
   test('should fail to register with existing username', async () => {
     // Registra un usuario primero
-    await request(app)
-      .post('/auth/register')
-      .send({
-        nombre: 'John',
-        apellido: 'Doe',
-        email: 'john.doe@example.com',
-        contraseña: 'password123',
-        username: 'john_doe',
-      });
+    await registerUser();
 
     // Intenta registrar otro usuario con el mismo username
-    const duplicateRegisterResponse = await request(app)
-      .post('/auth/register')
-      .send({
-        nombre: 'Jane',
-        apellido: 'Doe',
-        email: 'jane.doe@example.com',
-        contraseña: 'password456',
-        username: 'john_doe', // Mismo username que el usuario registrado anteriormente
-      });
+    const duplicateRegisterResponse = await registerUser();
 
     expect(duplicateRegisterResponse.statusCode).toBe(400);
-    expect(duplicateRegisterResponse.body).toHaveProperty('message', 'El nombre de usuario ya está en uso');
+    expect(duplicateRegisterResponse.body).toHaveProperty('error', 'El nombre de usuario ya está en uso');
   });
 
   test('should fail to log in with incorrect credentials', async () => {
     // Registra un usuario primero
-    await request(app)
-      .post('/auth/register')
-      .send({
-        nombre: 'John',
-        apellido: 'Doe',
-        email: 'john.doe@example.com',
-        contraseña: 'password123',
-        username: 'john_doe',
-      });
+    await registerUser();
 
     // Intenta iniciar sesión con credenciales incorrectas
     const incorrectLoginResponse = await request(app)
